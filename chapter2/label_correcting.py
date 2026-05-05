cddec16a380a19c9130aaa16fb35ca364d8e4003
Nam V. Do <namvdo11@gmail.com>
2026-05-05 12:23:08 +0300

    add label correcting impl

--- chapter2/label_correcting.py
+++ chapter2/label_correcting.py
@@ -1,3 +1,4 @@
+
 import heapq 
 
 from collections import deque
@@ -12,7 +13,6 @@ GValues: TypeAlias = dict[Node, Cost]
 Parent: TypeAlias = dict[Node, Node | None]
 
 
-
 SAMPLE_GRAPH = {
     'S': [('A', 4), ('B', 2)],
     'A': [('C', 3), ('G', 10)],
@@ -21,29 +21,28 @@ SAMPLE_GRAPH = {
     'G': [],
 }
 
-
-def dijkstra(graph: Graph, start: Node) -> tuple[GValues, Parent]:
+def label_correcting(graph: Graph, start: Node) -> tuple[GValues, Parent]:
     g: GValues = {node: float("inf") for node in graph}
     g[start] = 0 
     parent: Parent = {start: None}
-    pq: list[tuple[Cost, Node]] = [(0, start)] 
-    visited: set[Node] = set() 
+    in_queue: set[Node] = set([start])
 
-    while pq: 
-        cost, x = heapq.heappop(pq)
+    queue: deque[Node] = deque([start])
+    while queue: 
+        x = queue.popleft() 
+        in_queue.discard(x) 
 
-        if x in visited:
-            continue 
-
-        visited.add(x) 
-        for neighbor, edge_cost in graph[x]: 
-            new_cost = g[x] + edge_cost 
+        for neighbor, edge_cost in graph[x]:
+            new_cost = g[x] + edge_cost
             if new_cost < g[neighbor]:
                 g[neighbor] = new_cost 
                 parent[neighbor] = x 
-                heapq.heappush(pq, (new_cost, neighbor))
+                if neighbor not in in_queue:
+                    queue.append(neighbor)
+                    in_queue.add(neighbor)
+    
+    return g, parent
 
-    return g, parent 
 
 
 def reconstruct_path(parent: Parent, goal: Node) -> list[Node]: 
@@ -54,16 +53,11 @@ def reconstruct_path(parent: Parent, goal: Node) -> list[Node]:
         node = parent.get(node) 
     
     return list(reversed(path))
+    
 
-
-g, parent = dijkstra(SAMPLE_GRAPH, "S")
+print("Label correcting: ")
+g, parent = label_correcting(SAMPLE_GRAPH, "S")
 path = reconstruct_path(parent, "G")
-print("Dijkstra:")
-print(f"costs : { dict(g.items()) }")
+print(f"costs : {dict(g.items())}")
 print(f"path  : {' → '.join(path)}")
 print(f"total : {g['G']}\n")
-
-
-
-
-
